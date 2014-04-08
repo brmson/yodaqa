bcd9adf0f0fa460863ccddbe452f39471ec17628
Petr Baudis <pasky@ucw.cz>
2014-04-08 17:42:24 +0200

    YodaQAApp: Look at reader as generic CollectionReader_ImplBase

--- src/main/java/cz/brmlab/yodaqa/YodaQAApp.java
+++ src/main/java/cz/brmlab/yodaqa/YodaQAApp.java
@@ -7,6 +7,7 @@ import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CAS;
+import org.apache.uima.collection.CollectionReader_ImplBase;
 import org.apache.uima.resource.ResourceSpecifier;
 import org.apache.uima.util.XMLInputSource;
 
@@ -22,7 +23,7 @@ public class YodaQAApp {
 			AnalysisEngine ae = UIMAFramework.produceAnalysisEngine(specifier);
 			CAS cas = ae.newCAS();
 
-			InteractiveCollectionReader reader = new InteractiveCollectionReader();
+			CollectionReader_ImplBase reader = new InteractiveCollectionReader();
 			reader.initialize();
 			while (reader.hasNext()) {
 				reader.getNext(cas);
