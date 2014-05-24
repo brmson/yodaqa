f8d88bae5f52e18396522faeb3046eb2df4202ef
Petr Baudis <pasky@ucw.cz>
2014-05-24 20:11:17 +0200

    data/eval/tsvout-ranks: Include top generated answer in the listing

--- data/eval/tsvout-ranks.sh
+++ data/eval/tsvout-ranks.sh
@@ -3,4 +3,4 @@
 # List questions with correct answers considered, ordered by the correct
 # answer rank and numbered so that we can easily find the median etc.
 
-cut -f1,3,5,7 "$1" | sort -t $'\t' -k3 -n | grep -v '[-]1' | nl | less -F
+cut -f1,3,5,7,9 "$1" | sort -t $'\t' -k3 -n | grep -v '[-]1' | nl | less -F
