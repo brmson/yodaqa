3b30d5ecba671ced7798766ddc0607c484cc1ce0
Petr Baudis <pasky@ucw.cz>
2014-05-25 02:26:28 +0200

    SolrTerm.cluesToTerms(): Required only for ClueNE, ClueSV, ClueSubject

--- src/main/java/cz/brmlab/yodaqa/provider/solr/SolrTerm.java
+++ src/main/java/cz/brmlab/yodaqa/provider/solr/SolrTerm.java
@@ -5,7 +5,10 @@ import java.util.Collection;
 import java.util.List;
 
 import cz.brmlab.yodaqa.model.Question.Clue;
+import cz.brmlab.yodaqa.model.Question.ClueNE;
 import cz.brmlab.yodaqa.model.Question.CluePhrase;
+import cz.brmlab.yodaqa.model.Question.ClueSV;
+import cz.brmlab.yodaqa.model.Question.ClueSubject;
 
 public class SolrTerm {
 	protected String termStr;
@@ -50,7 +53,9 @@ public class SolrTerm {
 
 			String keyterm = clue.getCoveredText();
 			Double weight = clue.getWeight();
-			SolrTerm term = new SolrTerm(keyterm, weight, true /* FIXME */);
+			SolrTerm term = new SolrTerm(keyterm, weight,
+					(clue instanceof ClueNE) || (clue instanceof ClueSV) || (clue instanceof ClueSubject)
+				);
 			terms.add(term);
 		}
 
