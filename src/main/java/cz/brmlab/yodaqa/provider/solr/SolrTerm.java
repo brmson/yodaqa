184ebbb55f4f8d16eee8156d6a8ec424a7dd2905
Petr Baudis <pasky@ucw.cz>
2014-05-26 19:05:43 +0200

    Revert "SolrTerm.cluesToTerms(): Required only for ClueNE, ClueSV, ClueSubject"
    This reverts commit 3b30d5ecba671ced7798766ddc0607c484cc1ce0.
    Big performance loss.

--- src/main/java/cz/brmlab/yodaqa/provider/solr/SolrTerm.java
+++ src/main/java/cz/brmlab/yodaqa/provider/solr/SolrTerm.java
@@ -5,10 +5,7 @@ import java.util.Collection;
 import java.util.List;
 
 import cz.brmlab.yodaqa.model.Question.Clue;
-import cz.brmlab.yodaqa.model.Question.ClueNE;
 import cz.brmlab.yodaqa.model.Question.CluePhrase;
-import cz.brmlab.yodaqa.model.Question.ClueSV;
-import cz.brmlab.yodaqa.model.Question.ClueSubject;
 
 public class SolrTerm {
 	protected String termStr;
@@ -53,9 +50,7 @@ public class SolrTerm {
 
 			String keyterm = clue.getCoveredText();
 			Double weight = clue.getWeight();
-			SolrTerm term = new SolrTerm(keyterm, weight,
-					(clue instanceof ClueNE) || (clue instanceof ClueSV) || (clue instanceof ClueSubject)
-				);
+			SolrTerm term = new SolrTerm(keyterm, weight, true /* FIXME */);
 			terms.add(term);
 		}
 
