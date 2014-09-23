4857a4e1505159da2b58bd0261f759b494ab91f7
Petr Baudis <pasky@ucw.cz>
2014-09-23 20:57:05 +0200

    data/eval/tsvout-stats.sh: Don't assume a fixed total#=200 of answers

--- data/eval/tsvout-stats.sh
+++ data/eval/tsvout-stats.sh
@@ -5,9 +5,11 @@ showstats() {
 
 	# XXX: if no answer generated, the answer line may be
 	# completely missing
-	#total="$(wc -l <"$tsvout")"
-	total=200
-	reported="$(wc -l <"$tsvout")"
+	total="$(wc -l <"$tsvout")"
+	if [ "$total" -eq 0 ]; then
+		echo 'no answers generated'
+		return
+	fi
 
 	perfect="$(cat "$tsvout" | cut -f4 | grep '^1\.0' | wc -l)"
 	any="$(cat "$tsvout" | cut -f4 | grep -v '^0\.0$' | wc -l)"
@@ -16,8 +18,7 @@ showstats() {
 	anyp="$(echo "100*$any/$total" | bc -l)"
 
 	avgscore="$(echo "($(cat "$tsvout" | cut -f4 | tr '\n' '+')0)/$total" | bc -l)"
-	# XXX: We do not include non-reported questions in average time
-	avgtime="$(echo "($(cat "$tsvout" | cut -f2 | tr '\n' '+')0)/$reported" | bc -l)"
+	avgtime="$(echo "($(cat "$tsvout" | cut -f2 | tr '\n' '+')0)/$total" | bc -l)"
 
 	printf '%d/%d/%d %.1f%%/%.1f%% avgscore %.3f avgtime %.3f\n' \
 		"$perfect" "$any" "$total" "$perfectp" "$anyp" "$avgscore" "$avgtime"
