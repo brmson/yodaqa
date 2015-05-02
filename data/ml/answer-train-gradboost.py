b1c1446d39fc1fad780c751b0d253bf07aa5e6cf
Petr Baudis <pasky@ucw.cz>
2015-05-02 20:21:43 +0200

    data/ml/answer-train-gradboost.py: New alternative answer-train
    Not usable right now because dumping is not implemented and more
    importantly it doesn't deal correctly with class imbalance yet.

--- data/ml/answer-train-gradboost.py
+++ data/ml/answer-train-gradboost.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python -u
 #
-# Train a LogisticRegression classifier on the given answer TSV dataset.
+# Train a GradientBoostingClassifier on the given answer TSV dataset.
 #
-# Usage: answer-train-logistic.py <training-answer.tsv
+# Usage: answer-train-gradboost.py MODELPARAM... <training-answer.tsv
 #
 # Currently, this script trains a logistic regression classifier.
 # The output is a java code with the classifier configuration, to be
@@ -16,44 +16,35 @@
 
 import sys
 import time
-from sklearn import linear_model
+from sklearn import ensemble
+import joblib
 import numpy as np
 import numpy.random as random
 
 from answertrain import *
 
 
-def dump_weights(weights, labels):
-    for i in range(len(weights[0]) / 3):
-        print(' * %28s % 2.4f  %28s % 2.4f  %28s % 2.4f' %
-              (labels[i*3], weights[0][i*3],
-               labels[i*3 + 1], weights[0][i*3 + 1],
-               labels[i*3 + 2], weights[0][i*3 + 2]))
+class GBFactory:
+    def __init__(self, cfier_params):
+        self.cfier_params = cfier_params
 
-
-def dump_model(weights, labels, intercept):
-    for i in range(len(weights[0]) / 3):
-        # d01 is roughly estimated delta between feature not present and
-        # feature present and set to 1 - basically, the baseline influence
-        # of the feature (it has some meaning even for non-binary features)
-        d01 = weights[0][i*3] + weights[0][i*3 + 1] - weights[0][i*3 + 2]
-        print('\t/* %27s @,%%,! */ % 2.6f, % 2.6f, % 2.6f, /* %27s d01: % 2.6f */' %
-              (labels[i*3][1:],
-               weights[0][i*3], weights[0][i*3 + 1], weights[0][i*3 + 2],
-               labels[i*3][1:], d01))
-    print('/* intercept */ %f' % intercept)
-
-
-def cfier_factory(class_ratio, fv_train, class_train):
-    cfier = linear_model.LogisticRegression(class_weight={0: 1, 1: 0.5/class_ratio}, dual=False, fit_intercept=True)
-    cfier.fit(fv_train, class_train)
-    return cfier
+    def __call__(self, class_ratio, fv_train, class_train):
+        cfier = ensemble.GradientBoostingClassifier(**self.cfier_params)
+        cfier.fit(fv_train, class_train)
+        return cfier
 
 
 if __name__ == "__main__":
     # Seed always to the same number to get reproducible builds
     # TODO: Make this configurable on the command line or in the environment
     random.seed(17151713)
+
+    modelparams = sys.argv[1:]
+    cfier_opts = dict()
+    for p in modelparams:
+        k, v = p.split('=')
+        cfier_opts[k] = eval(v)
+    cfier_factory = GBFactory(cfier_opts)
 
     (answersets, labels) = load_answers(sys.stdin)
 
@@ -76,18 +67,16 @@ if __name__ == "__main__":
     t_end = time.clock()
     print('// training took %d seconds' % (t_end-t_start,))
 
+    # Store the model
+    modelfile = 'GradientBoostingClassifier-%s.pkl' % (','.join(modelparams),)
+    joblib.dump(cfier, modelfile, compress=3)
+    print('// model file ' + modelfile)
+
     # Report the test results - the individual accuracy metrics are obviously
     # not very meaningful as we test on the training data, but it can still
     # be informative wrt. the answerset metrics, esp. 'any good'.
     testres = test_model(cfier, fv_full, class_full, answersets, labels)
     print("// (full) " + test_msg(*testres))
 
-    # dump_weights(cfier.coef_, labels)
-
     print("// Full model is " + str(cfier).replace("\n", " "))
     print('//')
-    dump_model(cfier.coef_, labels, cfier.intercept_)
-
-    if False:
-        dump_answers(cfier, fv_full, class_full)
-        dump_weights(cfier.coef_, labels)
