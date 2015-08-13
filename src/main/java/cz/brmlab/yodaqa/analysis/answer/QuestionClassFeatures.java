3506c674ec056c1f9c4b2d90a39e4e49def22bcd
Jan Pichl <honza.pichl@gmail.com>
2015-08-13 15:38:48 +0200

    Added answer features for question class

---
+++ src/main/java/cz/brmlab/yodaqa/analysis/answer/QuestionClassFeatures.java
@@ -0,0 +1,48 @@
+package cz.brmlab.yodaqa.analysis.answer;
+
+import cz.brmlab.yodaqa.analysis.ansscore.AF;
+import cz.brmlab.yodaqa.analysis.ansscore.AnswerFV;
+import cz.brmlab.yodaqa.model.CandidateAnswer.AnswerFeature;
+import cz.brmlab.yodaqa.model.CandidateAnswer.AnswerInfo;
+import cz.brmlab.yodaqa.model.Question.QuestionClass;
+import cz.brmlab.yodaqa.model.Question.QuestionInfo;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by honza on 13.8.15.
+ */
+public class QuestionClassFeatures extends JCasAnnotator_ImplBase {
+	final Logger logger = LoggerFactory.getLogger(QuestionClassFeatures.class);
+
+	@Override
+	public void process(JCas jCas) throws AnalysisEngineProcessException {
+		JCas questionView, answerView;
+		try {
+			questionView = jCas.getView("Question");
+			answerView = jCas.getView("Answer");
+		} catch (Exception e) {
+			throw new AnalysisEngineProcessException(e);
+		}
+
+		QuestionClass qc = JCasUtil.selectSingle(questionView, QuestionClass.class);
+		String cls = qc.getQuestionClass();
+
+		AnswerInfo ai = JCasUtil.selectSingle(answerView, AnswerInfo.class);
+		AnswerFV fv = new AnswerFV(ai);
+		fv.setFeature(AF.QuestionClass_ClassName + cls, 1.0);
+
+		if (ai.getFeatures() != null)
+			for (FeatureStructure af : ai.getFeatures().toArray())
+				((AnswerFeature) af).removeFromIndexes();
+		ai.removeFromIndexes();
+		ai.setFeatures(fv.toFSArray(jCas));
+		ai.addToIndexes();
+	}
+}
