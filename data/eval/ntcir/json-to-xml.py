7f1c088f23c29978f6cfd599290e4b416fc8fae6
Silvicek <silvicek@gmail.com>
2015-08-27 19:08:19 +0200

    Added clueprint to questiondump+python script to output question_anaysis_result.xml files

---
+++ data/eval/ntcir/json-to-xml.py
@@ -0,0 +1,75 @@
+from __future__ import print_function 
+import xml.etree.cElementTree as ET
+import sys
+import json
+
+
+jfile=open("QuestionDump.json")
+xmlpath="QuestionDump.xml"
+
+
+TOPIC_SET=ET.Element("TOPIC_SET")
+METADATA=ET.SubElement(TOPIC_SET,"METADATA")
+ET.SubElement(METADATA,"RUN_ID").text="run_id here"
+ET.SubElement(METADATA,"DESCRIPTION").text="yodaqa"
+ANSWER_TYPE_SET_DEF=ET.SubElement(METADATA,"ANSWER_TYPE_SET_DEF")
+ET.SubElement(ANSWER_TYPE_SET_DEF,"PATH").text="path to answers"
+QUESTION_FORMAT_TYPE_DEF=ET.SubElement(METADATA,"ANSWER_TYPE_SET_DEF")
+ET.SubElement(QUESTION_FORMAT_TYPE_DEF,"PATH").text="path to questions"
+
+types=set()
+
+for line in jfile:
+#    print ("LINE",line)
+    jline=json.loads(line)
+    TOPIC=ET.SubElement(TOPIC_SET,"TOPIC",ID=jline['qId'])
+    QUESTION_ANALYSIS=ET.SubElement(TOPIC,"QUESTION_ANALYSIS")
+    QUESTION_FORMAT_TYPE_SET=ET.SubElement(QUESTION_ANALYSIS,"QUESTION_FORMAT_TYPE_SET")
+    ET.SubElement(QUESTION_FORMAT_TYPE_SET,"QUESTION_FORMAT_TYPE",RANK="",SCORE="")
+    ANSWER_TYPE_SET=ET.SubElement(QUESTION_ANALYSIS,"ANSWER_TYPE_SET")
+    jtypes=jline['LAT']
+    for i in range(0,len(jtypes)):
+        if jtypes[i]['specificity']=='0.0':
+            ET.SubElement(ANSWER_TYPE_SET,"ANSWER_TYPE",RANK="",SCORE="").text=jtypes[i]['text']
+            types.add(jtypes[i]['text'])
+    QUERY_SET=ET.SubElement(QUESTION_ANALYSIS,"QUERY_SET")
+    QUERY=ET.SubElement(QUERY_SET,"QUERY",ID="1")
+    KEY_TERM_SET=ET.SubElement(QUERY,"KEY_TERM_SET",LANGUAGE="EN")
+    jclues=jline['Clue']
+    for i in range(0,len(jclues)):
+        score=float(jclues[i]['clueWeight'])
+        if score<1:
+            score=1.0
+        ET.SubElement(KEY_TERM_SET,"KEY_TERM",RANK=str(i+1),SCORE=str(1-1/score)).text=jclues[i]['label']
+xml=ET.ElementTree(TOPIC_SET)
+xml.write("QuestionDump.xml")
+
+
+ANSWER_TYPE_SET_DEF=ET.Element("ANSWER_TYPE_SET_DEF")
+
+for t in types:
+    ET.SubElement(ANSWER_TYPE_SET_DEF,"ANSWER_TYPE_DEF",PARENT="ROOT",CHILDREN="$").text=t
+xmldef=ET.ElementTree(ANSWER_TYPE_SET_DEF)
+xmldef.write("typedef.xml")
+
+
+#<ANSWER_TYPE_SET_DEF>
+#  <ANSWER_TYPE_DEF PARENT="ROOT" CHILDREN="$">DEFINITION</ANSWER_TYPE_DEF>
+#  <ANSWER_TYPE_DEF PARENT="ROOT" CHILDREN="$">PERSON</ANSWER_TYPE_DEF>
+#  <ANSWER_TYPE_DEF PARENT="ROOT" CHILDREN="$">LOCATION</ANSWER_TYPE_DEF>
+#</ANSWER_TYPE_SET_DEF>
+#gs_root = ET.parse(sys.argv[2])
+#answer_map = {}
+#answers = gs_root.findall(".//answer_section")
+#for a in answers:
+#    ans = a.findall("answer_set/answer/expression_set/expression")
+#    id = a.findall("question_id")[0].text
+#    answer_map[id] = ans[0].text
+#
+#root = ET.parse(sys.argv[1])
+#questions = root.findall(".//question")
+#for q in questions:
+#    if (q.get("minimal") == "yes"):
+#    	s = q.get("id")[1:] + "\t" + q.get("answer_type") + "\t" + q.findall("instruction")[0].text.replace("\n","") + "\t" + str(answer_map[q.get("id")])
+#        print (s.encode('utf8'))
+#{"qId": "1669", "SV": [], "LAT": [{"synset": "4923519", "text": "property", "specificity": "-2.0", "type": "WordnetLAT"}, {"synset": "5009517", "text": "stature", "specificity": "0.0", "type": "WordnetLAT"}, {"synset": "24444", "text": "attribute", "specificity": "-3.0", "type": "WordnetLAT"}, {"synset": "5005153", "text": "bodily property", "specificity": "-1.0", "type": "WordnetLAT"}, {"text": "tall", "specificity": "0.0", "type": "LAT"}], "Clue": [{"label": "stature", "clueWeight": "1.5"}, {"label": "tall", "clueWeight": "1.5"}, {"label": "Mount McKinley", "clueWeight": "2.8000000000000003"}]"Concept": [{"fullLabel": "Mount McKinley", "cookedLabel": "Mount McKinley", "pageID": "207247"}]}
---
+++ src/main/java/cz/brmlab/yodaqa/io/debug/QuestionCluePrinter.java
@@ -0,0 +1,136 @@
+package cz.brmlab.yodaqa.io.debug;
+
+import cz.brmlab.yodaqa.model.Question.Clue;
+import cz.brmlab.yodaqa.model.Question.Concept;
+import cz.brmlab.yodaqa.model.Question.QuestionInfo;
+import cz.brmlab.yodaqa.model.Question.SV;
+import cz.brmlab.yodaqa.model.TyCor.LAT;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasConsumer_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+/**
+ * A consumer that displays the questions with a LAT dump to json file
+ * The format is:
+ * {"qId": "...", "SV": ["...", "..."], "LAT" : [ {"synset" : "...", "text" : "...", "specificity" : "..." "type" : "..."}, {...}, {...}]} \n
+ * Pair this with TSVQuestionReader or JSONQuestionReader e.g. on data/eval/.
+ */
+
+public class QuestionCluePrinter extends JCasConsumer_ImplBase {
+
+	public static final String PARAM_JSONFILE = "JSONFILE";
+	@ConfigurationParameter(name = PARAM_JSONFILE, mandatory = true)
+		private String JSONFile;
+	PrintWriter JSONOutput;
+
+
+	public synchronized void initialize(UimaContext context)
+		throws ResourceInitializationException {
+		super.initialize(context);
+
+		try {
+			JSONOutput = new PrintWriter(JSONFile);
+		} catch (IOException io) {
+			throw new ResourceInitializationException(io);
+		}
+	}
+
+	protected void output(String res)
+	{
+		//System.out.println(res);
+		JSONOutput.println(res);
+		JSONOutput.flush();
+	}
+
+	public synchronized void process(JCas jcas) throws AnalysisEngineProcessException {
+		JCas questionView;
+		try {
+			questionView = jcas;
+		} catch (Exception e) {
+			throw new AnalysisEngineProcessException(e);
+		}
+
+		QuestionInfo qi = JCasUtil.selectSingle(questionView, QuestionInfo.class);
+		/*{"qId": "...", "sv": "...", "LAT" : [ {...}, {...}, {...}]} */
+		String line = "{\"qId\": " + "\"" + qi.getQuestionId() + "\"" + ", " + "\"SV\": ";
+
+		String SVtmp ="[";
+		for (Iterator SVIterator = JCasUtil.select(jcas, SV.class).iterator(); SVIterator.hasNext(); ) {
+			SV sv = (SV) SVIterator.next();
+			SVtmp += "\"" + sv.getCoveredText() + "\"";
+			if(SVIterator.hasNext()){
+				SVtmp += ", ";
+			}
+		}
+		SVtmp += "], ";
+		line += SVtmp;
+
+		line += "\"LAT\": ";
+		String LATtmp = "[";
+		for (Iterator iterator = JCasUtil.select(jcas, LAT.class).iterator(); iterator.hasNext(); ) {
+			LAT l = (LAT) iterator.next();
+			/*{"synset" : "...", "text" : "...", "specificity" : "..." "type" : "..."}*/
+			LATtmp += "{";
+			if (l.getSynset() != 0) { //only add synset when it is not zero
+				LATtmp += "\"synset\": " + "\"" + l.getSynset() + "\", ";
+			}
+			//add the rest
+			LATtmp += "\"text\": \"" + l.getText() + "\"," + " \"specificity\": \"" + l.getSpecificity() + "\", " + "\"type\": " +
+				"\"" + l.getClass().getSimpleName() + "\"}";
+			//not last, add comma
+			if (iterator.hasNext()) {
+				LATtmp += ", ";
+			}
+		}
+		LATtmp += "], ";
+		line += LATtmp;
+
+		line += "\"Clue\": ";
+		String Cluetmp = "[";
+		for (Iterator iterator = JCasUtil.select(jcas, Clue.class).iterator(); iterator.hasNext(); ) {
+			Clue c = (Clue) iterator.next();
+			Cluetmp += "{";
+			Cluetmp += "\"label\": \"" + c.getLabel().replaceAll("\"", "") + "\", ";
+//			Concepttmp += "\"cookedLabel\": \"" + c.getCookedLabel().replaceAll("\"", "\\\"") + "\", ";
+			Cluetmp += "\"clueWeight\": \"" + c.getWeight() + "\"";
+			Cluetmp += "}";
+			//not last, add comma
+			if (iterator.hasNext()) {
+				Cluetmp += ", ";
+			}
+		}
+		Cluetmp += "],";
+		line += Cluetmp;
+
+		line += "\"Concept\": ";
+		String Concepttmp = "[";
+		for (Iterator iterator = JCasUtil.select(jcas, Concept.class).iterator(); iterator.hasNext(); ) {
+			Concept c = (Concept) iterator.next();
+			Concepttmp += "{";
+			Concepttmp += "\"fullLabel\": \"" + c.getFullLabel().replaceAll("\"", "\\\"") + "\", ";
+			Concepttmp += "\"cookedLabel\": \"" + c.getCookedLabel().replaceAll("\"", "\\\"") + "\", ";
+			Concepttmp += "\"pageID\": \"" + c.getPageID() + "\"";
+			Concepttmp += "}";
+//not last, add comma
+			if (iterator.hasNext()) {
+				Concepttmp += ", ";
+			}
+		}
+		Concepttmp += "]";
+		line += Concepttmp;
+
+
+		line += "}";
+		output(line);
+		//Question q = QuestionDashboard.getInstance().get(qi.getQuestionId());
+		//QuestionDashboard.getInstance().finishQuestion(q);
+	}
+}
