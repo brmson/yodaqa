111303bdedca59ab2bd3ecca826767049434e8c4
Jan Pichl <honza.pichl@gmail.com>
2015-09-02 14:08:35 +0200

    Modified script for exporting answers

--- data/eval/ntcir/generate_results.sh
+++ data/eval/ntcir/generate_results.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 question_dir=$1
+answer_sheet_dir=$2
 
 for dir in ${question_dir}/*; do
 	if [ -d "$dir" ]; then
@@ -10,8 +11,9 @@ for dir in ${question_dir}/*; do
 			python data/eval/ntcir/make-tsv.py $f > tmp.tsv
 			./gradlew tsvgs -PexecArgs="tmp.tsv output-${filename}.tsv" 2>&1 | tee data/eval/ntcir/log
 			rm -f tmp.tsv
+			python data/eval/ntcir/answers-to-xml.py output-${filename}.tsv ${answer_sheet_dir}/${filename}.xml > ${filename}-answer-sheet.xml
 			python data/eval/ntcir/log-to-xml.py data/eval/ntcir/log data/eval/ntcir/${filename}-results.xml
-			# rm -f data/eval/ntcir/log		
+			rm -f data/eval/ntcir/log		
 		done
 	fi
 	
