b79cb4485a1f14d8e7a126051f1f46b9db45aff3
Jan Pichl <honza.pichl@gmail.com>
2015-09-02 15:23:47 +0200

    Added readme for generating xml file for ntcir competition and changed output directory

--- data/eval/ntcir/dump_questions.sh
+++ data/eval/ntcir/dump_questions.sh
@@ -10,7 +10,7 @@ for dir in ${question_dir}/*; do
 			python data/eval/ntcir/make-tsv.py $f > tmp.tsv
 			./gradlew questionDump -PexecArgs="tmp.tsv tmp2.json"
 			rm -f tmp.tsv
-			python data/eval/ntcir/json-to-xml.py tmp2.json data/eval/ntcir/${filename}.xml	data/eval/ntcir/${filename}-typedef.xml
+			python data/eval/ntcir/json-to-xml.py tmp2.json data/eval/ntcir/output/${filename}.xml data/eval/ntcir/output/${filename}-typedef.xml
 			rm -f tmp2.json		
 		done
 	fi
--- data/eval/ntcir/generate_results.sh
+++ data/eval/ntcir/generate_results.sh
@@ -4,16 +4,17 @@ answer_sheet_dir=$2
 
 for dir in ${question_dir}/*; do
 	if [ -d "$dir" ]; then
+		echo ${dir##*/}
 	  	for f in ${dir}/*; do
 			filename="${f##*/}"
 			filename="${filename%.*}"
 			echo $filename
 			python data/eval/ntcir/make-tsv.py $f > tmp.tsv
-			./gradlew tsvgs -PexecArgs="tmp.tsv output-${filename}.tsv" 2>&1 | tee data/eval/ntcir/log
+			./gradlew tsvgs -PexecArgs="tmp.tsv data/eval/ntcir/output/output-${filename}.tsv" 2>&1 | tee data/eval/ntcir/output/log
 			rm -f tmp.tsv
-			python data/eval/ntcir/answers-to-xml.py output-${filename}.tsv ${answer_sheet_dir}/${filename}.xml > ${filename}-answer-sheet.xml
-			python data/eval/ntcir/log-to-xml.py data/eval/ntcir/log data/eval/ntcir/${filename}-results.xml
-			rm -f data/eval/ntcir/log		
+			python data/eval/ntcir/answers-to-xml.py data/eval/ntcir/output/output-${filename}.tsv ${answer_sheet_dir}/${dir##*/}/${filename}.xml > data/eval/ntcir/output/${filename}-answer-sheet.xml
+			python data/eval/ntcir/log-to-xml.py data/eval/ntcir/output/log data/eval/ntcir/output/${filename}-results.xml
+			rm -f data/eval/ntcir/output/log		
 		done
 	fi
 	
