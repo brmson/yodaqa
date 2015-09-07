3ca8dde67d2fa430fc7c176b68017b13ac2bdfde
Silvicek <silvicek@gmail.com>
2015-09-07 17:00:49 +0200

    PassageLogScore is not a log

--- src/main/java/cz/brmlab/yodaqa/analysis/passage/CandidateGenerator.java
+++ src/main/java/cz/brmlab/yodaqa/analysis/passage/CandidateGenerator.java
@@ -35,7 +35,7 @@ public abstract class CandidateGenerator extends JCasAnnotator_ImplBase {
 
 
 		fv.setFeature(AF.Occurences, 1.0);
-		fv.setFeature(AF.PassageLogScore, Math.log(1 + p.getScore()));
+		fv.setFeature(AF.PassageLogScore, p.getScore());
 
 		for (QuestionLATMatch qlm : JCasUtil.selectCovered(QuestionLATMatch.class, p)) {
 			double distance = 1000;
