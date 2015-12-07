702f88eb68a35bc72da21bb4a65bebb421cfe841
Petr Baudis <pasky@ucw.cz>
2015-12-07 18:48:36 +0100

    question FocusGenerator: Further improve handling of what-questions
    What is the last movie that X Y made? -> 'movie', not 'what'

--- src/main/java/cz/brmlab/yodaqa/analysis/question/FocusGenerator.java
+++ src/main/java/cz/brmlab/yodaqa/analysis/question/FocusGenerator.java
@@ -118,11 +118,12 @@ public class FocusGenerator extends JCasAnnotator_ImplBase {
 			}
 		}
 
-		/* Wh- DOBJ is preferrable to NSUBJ, if available.
+		/* Wh- DOBJ is preferrable to NSUBJ, if available and not be-bound.
 		 * "Who did X Y play in Z?" -> DOBJ:who (and NSUBJ:X Y) */
 		if (focus == null) {
 			for (DOBJ dobj : JCasUtil.selectCovered(DOBJ.class, sentence)) {
-				if (dobj.getDependent().getPos().getPosValue().matches("^W.*")) {
+				if (dobj.getDependent().getPos().getPosValue().matches("^W.*")
+				    && !LATByFocus.isAmbiguousQLemma(dobj.getDependent().getLemma().getValue().toLowerCase())) {
 					focusTok = dobj.getDependent();
 					focus = focusTok;
 					logger.debug("DOBJ+W {}", focus.getCoveredText());
