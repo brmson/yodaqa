43c197f9ea44a84bf0b1aff842627a18b8415b30
Jiri Nadvornik <nadvornik.jiri@gmail.com>
2015-12-26 14:40:16 +0100

    Fix of previous pasky's fix. I have no idea why this is correct combination of backslashes, but it seems to be working.

--- src/main/java/cz/brmlab/yodaqa/io/debug/QuestionPrinter.java
+++ src/main/java/cz/brmlab/yodaqa/io/debug/QuestionPrinter.java
@@ -141,8 +141,9 @@ public class QuestionPrinter extends JCasConsumer_ImplBase {
 			Concepttmp += "\"labelProbability\": " + c.getLabelProbability() + ", ";
 			Concepttmp += "\"logPopularity\": " + c.getLogPopularity() + ", ";
 			Concepttmp += "\"score\": " + c.getScore() + ", ";
-			if (c.getDescription() != null)
-				Concepttmp += "\"description\": \"" + c.getDescription().replaceAll("\\", "\\\\\\\\").replaceAll("\"", "\\\\\"") + "\", ";
+			if (c.getDescription() != null){
+				Concepttmp += "\"description\": \"" + c.getDescription().replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\\\"") + "\", ";
+			}
 			Concepttmp += "\"relatedness\": " + c.getRelatedness() + ", ";
 			Concepttmp += "\"getByLAT\": " + (c.getByLAT() ? 1 : 0) + ", ";
 			Concepttmp += "\"getByNE\": " + (c.getByNE() ? 1 : 0) + ", ";
