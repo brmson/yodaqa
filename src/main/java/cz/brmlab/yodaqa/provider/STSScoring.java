e64cd4b075d6d550e09f11d580a0b14804cece50
Petr Baudis <pasky@ucw.cz>
2016-03-31 19:49:59 +0200

    provider.STSScoring: New class, originally by Honza Pichl
    Honza Pichl had this as RNNScoring in analysis.rdf on his branch.
    I just moved this to a more appropriate place, renamed and added class
    description.

---
+++ src/main/java/cz/brmlab/yodaqa/provider/STSScoring.java
@@ -0,0 +1,73 @@
+package cz.brmlab.yodaqa.provider;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.stream.JsonReader;
+
+import java.io.*;
+import java.net.*;
+import java.util.List;
+import java.util.Map;
+
+/** Wrapper around a deep learning based "STS" sentence pair scorer API.
+ * Talks to https://github.com/brmson/dataset-sts scoring-api tool,
+ * asking it to use its models to score pairs of token sequences - in our
+ * case, that'd be, say, tuples of (question, passage) or (question,
+ * proplabel).  dataset-sts implements a variety of models, from BM25 TFIDF
+ * to advanced attention-based deep neural networks. */
+public class STSScoring {
+	private static final String MODEL_URL = "http://pichl.ailao.eu:5000/score";
+
+	public static List<Double> getScores(String question, List<String> labels, int propertyNumber) {
+		List<Double> res = null;
+		try {
+			URL url = new URL(MODEL_URL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+
+			String input = buildRequestBody(question, labels, propertyNumber);
+
+			OutputStream os = conn.getOutputStream();
+			os.write(input.getBytes());
+			os.flush();
+
+			res = processResponse(conn.getInputStream());
+//			BufferedReader br = new BufferedReader(new InputStreamReader(
+//					(conn.getInputStream())));
+
+//			String output;
+//			System.out.println("Output from Server .... \n");
+//			while ((output = br.readLine()) != null) {
+//				System.out.println(output);
+//			}
+			conn.disconnect();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	private static String buildRequestBody(String question, List<String> labels, int propertyNumber) {
+		JsonObject jobject = new JsonObject();
+		jobject.addProperty("question", question);
+		JsonArray propLabels = new JsonArray();
+		for(String lab: labels) {
+			propLabels.add(new JsonPrimitive(lab));
+		}
+		jobject.add("prop_labels", propLabels);
+		jobject.addProperty("property_number", propertyNumber);
+		return jobject.toString();
+	}
+
+	@SuppressWarnings("unchecked")
+	private static List<Double> processResponse (InputStream stream) {
+		GsonBuilder builder = new GsonBuilder();
+		Map<String, List<Double>> json = builder.create().fromJson(new InputStreamReader(stream), Map.class);
+		return json.get("score");
+	}
+}
