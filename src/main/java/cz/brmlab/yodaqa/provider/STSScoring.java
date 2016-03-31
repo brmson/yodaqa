07d75b0246212e42e12efffe4b439f4881ecded4
Petr Baudis <pasky@ucw.cz>
2016-03-31 19:57:49 +0200

    STSScoring.getScores(): Retry mechanism as with all other APIs

--- src/main/java/cz/brmlab/yodaqa/provider/STSScoring.java
+++ src/main/java/cz/brmlab/yodaqa/provider/STSScoring.java
@@ -1,10 +1,11 @@
 package cz.brmlab.yodaqa.provider;
 
+import java.util.concurrent.TimeUnit;
+
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
-import com.google.gson.stream.JsonReader;
 
 import java.io.*;
 import java.net.*;
@@ -16,40 +17,51 @@ import java.util.Map;
  * asking it to use its models to score pairs of token sequences - in our
  * case, that'd be, say, tuples of (question, passage) or (question,
  * proplabel).  dataset-sts implements a variety of models, from BM25 TFIDF
- * to advanced attention-based deep neural networks. */
+ * to advanced attention-based deep neural networks.
+ *
+ * TODO: Singleton rather than static? */
 public class STSScoring {
 	private static final String MODEL_URL = "http://pichl.ailao.eu:5000/score";
 
 	public static List<Double> getScores(String question, List<String> labels, int propertyNumber) {
 		List<Double> res = null;
-		try {
-			URL url = new URL(MODEL_URL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
-
-			String input = buildRequestBody(question, labels, propertyNumber);
-
-			OutputStream os = conn.getOutputStream();
-			os.write(input.getBytes());
-			os.flush();
-
-			res = processResponse(conn.getInputStream());
-//			BufferedReader br = new BufferedReader(new InputStreamReader(
-//					(conn.getInputStream())));
-
-//			String output;
-//			System.out.println("Output from Server .... \n");
-//			while ((output = br.readLine()) != null) {
-//				System.out.println(output);
-//			}
-			conn.disconnect();
-
-		} catch (IOException e) {
-			e.printStackTrace();
+		while (true) {
+			try {
+				res = getScoresDo(question, labels, propertyNumber);
+				break; // Success!
+			} catch (IOException e) {
+				notifyRetry(e);
+			}
 		}
 		return res;
+	}
+
+	protected static List<Double> getScoresDo(String question, List<String> labels, int propertyNumber) throws IOException {
+		URL url = new URL(MODEL_URL);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+
+		String input = buildRequestBody(question, labels, propertyNumber);
+
+		OutputStream os = conn.getOutputStream();
+		os.write(input.getBytes());
+		os.flush();
+
+		List<Double> res = processResponse(conn.getInputStream());
+		conn.disconnect();
+		return res;
+	}
+
+	protected static void notifyRetry(Exception e) {
+		e.printStackTrace();
+		System.err.println("*** " + MODEL_URL + " STS Query (temporarily?) failed, retrying in a moment...");
+		try {
+			TimeUnit.SECONDS.sleep(10);
+		} catch (InterruptedException e2) { // oof...
+			e2.printStackTrace();
+		}
 	}
 
 	private static String buildRequestBody(String question, List<String> labels, int propertyNumber) {
