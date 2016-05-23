ccd60ada8dd55cc9b4795e80dbbc158ceda3b96e
k0105 <falk.pollok@rwth-aachen.de>
2016-05-23 13:26:13 -0400

    Added option to load and override backend URLs.

--- src/main/java/cz/brmlab/yodaqa/provider/rdf/DBpediaLookup.java
+++ src/main/java/cz/brmlab/yodaqa/provider/rdf/DBpediaLookup.java
@@ -1,12 +1,14 @@
 package cz.brmlab.yodaqa.provider.rdf;
 
+import cz.brmlab.yodaqa.provider.UrlManager;
+
 /** This is an abstract base class for accessing DBpedia. */
 
 public abstract class DBpediaLookup extends CachedJenaLookup {
 	public DBpediaLookup() {
 		/* Replace the first URL below with http://dbpedia.org/sparql
 		 * to use the public DBpedia SPARQL endpoint. */
-		super("http://dbpedia.ailao.eu:3030/dbpedia/query",
+		super(UrlManager.lookUpUrl(UrlManager.DataBackends.DBPEDIA.ordinal()),//"http://127.0.0.1:3037/dbpedia/query",
 			"PREFIX : <http://dbpedia.org/resource/>\n" +
 			"PREFIX dbpedia2: <http://dbpedia.org/property/>\n" +
 			"PREFIX dbpedia: <http://dbpedia.org/>\n" +
