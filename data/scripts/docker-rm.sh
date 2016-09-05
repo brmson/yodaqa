9ba2825a6cbc9113e8addeba33a5a28462000e6c
root <root@docker>
2016-09-05 13:25:12 +0200

    Added description of purpose

--- data/scripts/docker-rm.sh
+++ data/scripts/docker-rm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Removes all exited containers and images whose build failed
 docker rm $(docker ps -a -q -f status=exited)
 docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
