ae2cfdd279433811b933cc556d388fb34e7b9766
Jan Pichl <honza.pichl@gmail.com>
2018-02-16 12:46:49 +0100

    Run repair-json script in split_data.sh

--- data/ml/diffbotfbpath/split_data.sh
+++ data/ml/diffbotfbpath/split_data.sh
@@ -10,12 +10,31 @@ filename=$(basename "$file_path")
 extension="${filename##*.}"
 filename="${filename%.*}"
 
+repair() {	
+	python data/ml/repair-json.py $1 > /tmp/repaired.json
+	mv /tmp/repaired.json $1
+}
+
 if [[ ! -z "${val_size}" ]]
 then
 	head -${train_size} ${file_path} > ${out_dir}/$filename-train.$extension
 	tail -n +$((train_size + 1)) ${file_path} | head -${val_size} > ${out_dir}/$filename-val.$extension
-	tail -n +$((train_size + val_size + 1)) ${file_path} >  ${out_dir}/$filename-test.$extension
+	tail -n +$((train_size + val_size + 1)) ${file_path} > ${out_dir}/$filename-test.$extension
+	if [[ "$extension" == "json" ]]
+	then
+		for split in train val test
+		do
+			repair ${out_dir}/$filename-$split.$extension
+		done
+	fi
 else
 	head -${train_size} ${file_path} > ${out_dir}/$filename-train.$extension
-	tail -n +$((train_size + 1)) ${file_path} >  ${out_dir}/$filename-test.$extension
+	tail -n +$((train_size + 1)) ${file_path} > ${out_dir}/$filename-test.$extension
+	if [[ "$extension" == "json" ]]
+	then
+		for split in train test
+		do
+			repair ${out_dir}/$filename-$split.$extension
+		done
+	fi
 fi
