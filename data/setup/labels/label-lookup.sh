0e0abb7c829192a9cab90b51d45976c4aaebfcb5
OpsWorks user matulma4 <matulma4@scraper.localdomain>
2018-06-28 08:02:58 +0000

    Fixed URL to dictionary file; Added archive extraction

--- data/setup/labels/label-lookup.sh
+++ data/setup/labels/label-lookup.sh
@@ -2,18 +2,21 @@
 username=$(whoami)
 # Create directory if does not exist
 mkdir /home/$username/docker/data
+cp *.sh /home/$username/docker/data
 cd /home/$username/docker/data
 
 # Clone repo
 git clone https://github.com/brmson/label-lookup.git
 
 cd label-lookup
-
+mv ../*.sh .
 # Download data
 wget http://downloads.dbpedia.org/2014/en/labels_en.nt.bz2
 wget http://downloads.dbpedia.org/2014/en/page_ids_en.nt.bz2
 wget http://downloads.dbpedia.org/2014/en/redirects_transitive_en.nt.bz2
 
+
+bzip2 -d *.bz2
 # Prepare data
 ./preprocess.py labels_en.nt page_ids_en.nt redirects_transitive_en.nt sorted_list.dat
 
